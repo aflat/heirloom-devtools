1a543bac6488ce0a70919ce7ee85d5504878e531
George Stockfisch <gstock.public@gmail.com>
2015-10-28 12:58:32 -0400

    hardcode a stropts.h file location, detect a install executable

--- make/src/Makefile.mk
+++ make/src/Makefile.mk
@@ -4,7 +4,7 @@ OBJ = ar.o depvar.o dist.o dmake.o doname.o dosys.o files.o globals.o \
 	implicit.o macro.o main.o make.o misc.o nse.o nse_printdep.o \
 	parallel.o pmake.o read.o read2.o rep.o state.o getopt.o version.o
 
-XFLAGS = -I../include -D_GNU_SOURCE -DSUNOS4_AND_AFTER \
+XFLAGS = -I/usr/lib/x86_64-redhat-linux5E/include -I../include -D_GNU_SOURCE -DSUNOS4_AND_AFTER \
 	-DHDRSDIR='"$(HDRSDIR)"' \
 	-DSHELL='"$(SHELL)"' -DPOSIX_SHELL='"$(POSIX_SHELL)"'
 
